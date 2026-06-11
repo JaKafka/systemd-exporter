9be7d9c8a411973f03db4c738c8072ac96e79db1
JaKafka <152932575+JaKafka@users.noreply.github.com.>
2026-06-12 00:18:39 +0200

    expose the metrics

--- cmd/systemd-exporter/main.go
+++ cmd/systemd-exporter/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"text/tabwriter"
+	"time"
 
 	"github.com/JaKafka/systemd-exporter/internal/systemd"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // version is stamped at build time via -ldflags "-X main.version=<tag>".
@@ -18,6 +23,7 @@ var version = "dev"
 
 func main() {
 	logLevel := flag.String("log-level", "info", "log level: debug, info, warn, error")
+	listenAddr := flag.String("web.listen-address", ":9558", "address to expose the /metrics endpoint on")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
@@ -25,15 +31,60 @@ func main() {
 	}))
 	slog.SetDefault(logger)
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	switch flag.Arg(0) {
 	case "observe":
-		ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-		defer stop()
 		runObserveAll(ctx)
+	case "", "serve":
+		runServe(ctx, *listenAddr)
 	default:
-		// TODO(JK): add server here
-		slog.Error("server not implemented yet")
+		slog.Error("unknown command", "command", flag.Arg(0))
 		os.Exit(1)
+	}
+}
+
+func runServe(ctx context.Context, addr string) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	slog.Info("starting", "version", version)
+
+	collector, err := systemd.New(ctx)
+	if err != nil {
+		slog.Error("connect to systemd D-Bus", "err", err)
+		os.Exit(1)
+	}
+	defer collector.Close()
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(systemd.NewExporter(collector))
+
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		slog.Info("serving metrics", "addr", addr, "path", "/metrics")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("http server", "err", err)
+			cancel()
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("graceful shutdown", "err", err)
 	}
 }
 
